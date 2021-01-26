b7af9a1e4d2c8b2b2d936a37bb03b1dbe997f928
ViktoriaOhorilko <viktoriaohorilko@gmail.com>
2021-01-26 19:18:40 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View, Button, Dimensions, TouchableOpacity } from "react-native";
+import StartImage from './assets/background_image.png';
+
+const Ww = Dimensions.get('window').width;
+const Wh = Dimensions.get('window').height;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Sonia starts working on Geoclock!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={StartImage} style={styles.image}>
+        {/*<Text style={styles.texttry}>GEOCLOCK</Text>*/}
+        <TouchableOpacity  style={styles.sign_in}>
+          <Text style={styles.text}>Sign in</Text>
+        </TouchableOpacity>
+        <TouchableOpacity  style={styles.sign_up}>
+          <Text style={styles.text}>Sign up</Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 }
@@ -14,8 +25,61 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e8fcc8',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column"
   },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
+  },
+  text: {
+    color: "white",
+    fontSize: 14,
+    textAlign: "center"
+  },
+  texttry: {
+    marginTop: 500,
+    color: "white",
+    fontSize: 31,
+    textAlign: "center",
+    //fontFamily: "sans-serif-medium",
+    fontFamily: "monospace",
+    letterSpacing: 6,
+    fontWeight: "600",
+
+    lineHeight: 50,
+
+    //textShadowColor:'white',
+    //textShadowOffset:{width: 0, height: 0},
+    //textShadowRadius:3
+    //font
+    //fontFamily: "notoserif"
+    //fontFamily: "sans-serif"
+    //fontFamily: "Roboto"
+  },
+  sign_in:{
+    height: 40,
+    borderRadius: 33,
+    borderColor: "#E9ECF7",
+    borderWidth: 2,
+    marginTop: (Wh/6*4),
+    marginLeft: (Ww-297)/2,
+    width: 297,
+    backgroundColor: "transparent",
+    zIndex: 1,
+    justifyContent: 'center'
+  },
+  sign_up:{
+    height: 40,
+    borderRadius: 33,
+    borderColor: "#E9ECF7",
+    borderWidth: 2,
+    marginTop: (527-470-40),
+    marginLeft: (Ww-297)/2,
+    width: 297,
+    backgroundColor: "transparent",
+    zIndex: 1,
+    justifyContent: 'center'
+  }
+
 });
